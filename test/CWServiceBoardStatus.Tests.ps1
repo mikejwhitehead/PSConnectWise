ac60e01853cdf50fd3bf8a5df6ec7cf54484146a
Brian <brian.k.ojeda@gmail.com>
2016-04-18 09:50:31 -0400

    updated the unit test to use the connection info objc

--- test/CWServiceBoardStatus.Tests.ps1
+++ test/CWServiceBoardStatus.Tests.ps1
@@ -7,19 +7,22 @@ Describe 'CWServiceBoardStatus' {
 
 	. "$WorkspaceRoot\test\LoadTestSettings.ps1"
 	
-	Context 'Get-CWServiceBoardStatus' {
+	# get the server connnection
+	$pstrServer = Get-CWConnectionInfo -Domain $pstrSvrDomain -CompanyName $pstrSvrCompany -PublicKey $pstrSvrPublic -PrivateKey $pstrSvrPrivate;
+	
+	Context 'Get-CWServiceBoardStatus w/o ConnectionInfo Obj' {
 		
 		$pstrBoardID = $pstrGenSvc.boardIds[0];
 		
 		It 'gets board status and check that the results is an array' {
 			$boardID = $pstrBoardID;
-			$statuses = Get-CWServiceBoardStatus -BoardID $boardID -Domain $pstrSvrDomain -CompanyName $pstrSvrCompany -PublicKey $pstrSvrPublic -PrivateKey $pstrSvrPrivate;
+			$statuses = Get-CWServiceBoardStatus -BoardID $boardID -Server $pstrServer;
 			$statuses.GetType().BaseType.Name | Should Be "Array";		
 		}
 		
 		It 'gets board and pipes it through the Select-Object cmdlet for the id property of the first object' {
 			$boardID = $pstrBoardID;
-			$status = Get-CWServiceBoardStatus -BoardID $boardID -Domain $pstrSvrDomain -CompanyName $pstrSvrCompany -PublicKey $pstrSvrPublic -PrivateKey $pstrSvrPrivate | Select-Object boardId -First 1;
+			$status = Get-CWServiceBoardStatus -BoardID $boardID -Server $pstrServer | Select-Object boardId -First 1;
 			$status | Select-Object -ExpandProperty boardId | Should Be $boardID;		
 		}
 	
