5a1ecc71793faea751e316418539999a681b2709
Brian <brian.k.ojeda@gmail.com>
2016-04-18 09:50:10 -0400

    updated the parameter sets and to accept connection info obj

--- src/CWServiceBoardStatusCmdLets.psm1
+++ src/CWServiceBoardStatusCmdLets.psm1
@@ -4,35 +4,34 @@
 function Get-CWServiceBoardStatus
 {
     [CmdLetBinding()]
+    [OutputType("PSObject[]", ParameterSetName="Normal")]
+    [OutputType("PSObject", ParameterSetName="Single")]
     param
     (
-        [Parameter(ParameterSetName='SingleBoard', Position=0, Mandatory=$true, ValueFromPipeline=$true)]
+        [Parameter(ParameterSetName='Normal', Position=0, Mandatory=$true, ValueFromPipeline=$true)]
         [ValidateNotNullOrEmpty()]
         [uint32]$BoardID,
-        [Parameter(ParameterSetName='SingleBoard', Position=2, Mandatory=$true)]
-        [Parameter(ParameterSetName='BoardQuery', Position=1, Mandatory=$true)]
+        [Parameter(ParameterSetName='Normal', Position=2, Mandatory=$true)]
+        [Parameter(ParameterSetName='Query', Position=2, Mandatory=$true)]
         [ValidateNotNullOrEmpty()]
-        [string]$Domain,
-        [Parameter(ParameterSetName='SingleBoard', Position=3, Mandatory=$true)]
-        [Parameter(ParameterSetName='BoardQuery', Position=2, Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [string]$CompanyName,
-        [Parameter(ParameterSetName='SingleBoard', Position=4, Mandatory=$true)]
-        [Parameter(ParameterSetName='BoardQuery', Position=3, Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [string]$PublicKey,
-        [Parameter(ParameterSetName='SingleBoard', Position=5, Mandatory=$true)]
-        [Parameter(ParameterSetName='BoardQuery', Position=4, Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [string]$PrivateKey
+        [PSObject]$Server
     )
     
     Begin
     {
         $MAX_ITEMS_PER_PAGE = 50;
+        [CwApiServiceBoardStatusSvc] $BoardStatusSvc = $null; 
         
-        # get the Board service
-        $BoardStatusSvc = [CwApiServiceBoardStatusSvc]::new($Domain, $CompanyName, $PublicKey, $PrivateKey);
+        # get the Company service
+        if ($Server -ne $null)
+        {
+            $BoardStatusSvc = [CwApiServiceBoardStatusSvc]::new($Server);
+        } 
+        else 
+        {
+            # TODO: determine whether or not to keep this as an option
+            $BoardStatusSvc = [CwApiServiceBoardStatusSvc]::new($Domain, $CompanyName, $PublicKey, $PrivateKey);
+        }
         
         [uint32] $statusCount = $MAX_ITEMS_PER_PAGE;
         [uint32] $pageCount  = 1;
