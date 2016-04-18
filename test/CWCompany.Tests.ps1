9106bf4d84b966b5ea50082142dde462ee7ba09f
Brian <brian.k.ojeda@gmail.com>
2016-04-18 09:42:05 -0400

    added missing semicolons

--- test/CWCompany.Tests.ps1
+++ test/CWCompany.Tests.ps1
@@ -26,20 +26,20 @@ Describe 'CWCompany' {
 		
 		It 'gets company and pipes it through the Select-Object cmdlet for the id property' {
 			$companyID = $pstrCompanyID;
-			$company = Get-CWCompany -ID $companyID -Server $pstrServer
+			$company = Get-CWCompany -ID $companyID -Server $pstrServer;
 			$company | Select-Object -ExpandProperty id | Should Be $companyID;		
 		}
 		
 		It 'gets the id and subject properties of a company by using the -Property param' {
 			$companyID = $pstrCompanyID;
 			$fields = @("id", "identifier", "name");
-			$company = Get-CWCompany -ID $companyID -Property $fields -Server $pstrServer
+			$company = Get-CWCompany -ID $companyID -Property $fields -Server $pstrServer;
 			$company.PSObject.Properties | Measure-Object | Select -ExpandProperty Count | Should Be $fields.Count;		
 		}
 		
 		It 'gets tickets by passing array of company ids to the -ID param' {
 			$companyIDs = $pstrCompanyIDs;
-			$tickets = Get-CWCompany -ID $companyIDs -Server $pstrServer
+			$tickets = Get-CWCompany -ID $companyIDs -Server $pstrServer;
 			$tickets | Measure-Object | Select -ExpandProperty Count | Should Be $companyIDs.Count;		
 		}
 		
@@ -51,20 +51,20 @@ Describe 'CWCompany' {
 		
 		It 'gets company based on the -Filter param' {
 			$filter = "id = $pstrCompanyID";
-			$company = Get-CWCompany -Filter $filter -Server $pstrServer
+			$company = Get-CWCompany -Filter $filter -Server $pstrServer;
 			$company.id | Should Be $pstrCompanyID;		
 		}
 		
 		It 'gets company based on the -Filter param and uses the SizeLimit param' {
 			$filter = "id IN ($([String]::Join(',', $pstrCompanyIDs)))";
 			$sizeLimit =  2;
-			$tickets = Get-CWCompany -Filter $filter -SizeLimit $sizeLimit -Server $pstrServer
+			$tickets = Get-CWCompany -Filter $filter -SizeLimit $sizeLimit -Server $pstrServer;
 			$tickets | Measure-Object | Select -ExpandProperty Count | Should Be $sizeLimit;
 		}
 		
 		It 'gets tickets and sorts company id by descending piping cmdlet through Sort-Object cmdlet' {
 			$companyIDs = $pstrCompanyIDs;
-			$tickets = Get-CWCompany -ID $companyIDs -Domain $pstrSvrDomain -CompanyName $pstrSvrCompany -PublicKey $pstrSvrPublic -PrivateKey $pstrSvrPrivate | Sort -Descending id;
+			$tickets = Get-CWCompany -ID $companyIDs -Server $pstrServer | Sort -Descending id;
 			$maxTicketId = $companyIDs | Measure-Object -Maximum | Select -ExpandProperty Maximum
 			$tickets[0].id | Should Be $maxTicketId;		
 		}
