38305274ef9fee1774eea6bbafa0f25a1c0fd886
Brian <brian.k.ojeda@gmail.com>
2016-04-18 09:41:45 -0400

    updated the unit test to pass the connection info object

--- test/CWServiceBoard.Tests.ps1
+++ test/CWServiceBoard.Tests.ps1
@@ -7,6 +7,9 @@ Describe 'CWServiceBoard' {
 		
 	. "$WorkspaceRoot\test\LoadTestSettings.ps1"
 	
+	# get the server connnection
+	$pstrServer = Get-CWConnectionInfo -Domain $pstrSvrDomain -CompanyName $pstrSvrCompany -PublicKey $pstrSvrPublic -PrivateKey $pstrSvrPrivate;
+	
 	Context 'Get-CWServiceBoard' {
 		
 		$pstrBoardIDs = $pstrGenSvc.boardIds;
@@ -14,44 +17,44 @@ Describe 'CWServiceBoard' {
 	
 		It 'gets board and checks for the id field' {
 			$boardID = $pstrBoardID;
-			$board = Get-CWServiceBoard -BoardID $boardID -Domain $pstrSvrDomain -CompanyName $pstrSvrCompany -PublicKey $pstrSvrPublic -PrivateKey $pstrSvrPrivate;
+			$board = Get-CWServiceBoard -BoardID $boardID -Server $pstrServer;
 			$board.id | Should Be $boardID;		
 		}
 		
 		It 'gets board and pipes it through the Select-Object cmdlet for the id property' {
 			$boardID = $pstrBoardID;
-			$board = Get-CWServiceBoard -BoardID $boardID -Domain $pstrSvrDomain -CompanyName $pstrSvrCompany -PublicKey $pstrSvrPublic -PrivateKey $pstrSvrPrivate;
+			$board = Get-CWServiceBoard -BoardID $boardID -Server $pstrServer;
 			$board | Select-Object -ExpandProperty id | Should Be $boardID;		
 		}
 		
 		It 'gets boards by passing array of board ids to the -BoardID param' {
 			$boardIDs = $pstrBoardIDs;
-			$boards = Get-CWServiceBoard -BoardID $boardIDs -Domain $pstrSvrDomain -CompanyName $pstrSvrCompany -PublicKey $pstrSvrPublic -PrivateKey $pstrSvrPrivate;
+			$boards = Get-CWServiceBoard -BoardID $boardIDs -Server $pstrServer;
 			$boards | Measure-Object | Select -ExpandProperty Count | Should Be $boardIDs.Count;		
 		}
 		
 		It 'gets list of boards that were piped to the cmdlet' {
 			$boardIDs = $pstrBoardIDs;
-			$boards = $boardIDs | Get-CWServiceBoard -Domain $pstrSvrDomain -CompanyName $pstrSvrCompany -PublicKey $pstrSvrPublic -PrivateKey $pstrSvrPrivate;
+			$boards = $boardIDs | Get-CWServiceBoard -Server $pstrServer;
 			$boards | Measure-Object | Select -ExpandProperty Count | Should Be $boardIDs.Count;		
 		}
 		
 		It 'gets board based on the -Filter param' {
 			$filter = "id = $pstrBoardID";
-			$board = Get-CWServiceBoard -Filter $filter -Domain $pstrSvrDomain -CompanyName $pstrSvrCompany -PublicKey $pstrSvrPublic -PrivateKey $pstrSvrPrivate;
+			$board = Get-CWServiceBoard -Filter $filter -Server $pstrServer;
 			$board.id | Should Be $pstrBoardID;		
 		}
 		
 		It 'gets board based on the -Filter param and uses the SizeLimit param' {
 			$filter = "id IN ($([String]::Join(",", $pstrBoardIDs)))";
 			$sizeLimit =  2;
-			$boards = Get-CWServiceBoard -Filter $filter -SizeLimit $sizeLimit -Domain $pstrSvrDomain -CompanyName $pstrSvrCompany -PublicKey $pstrSvrPublic -PrivateKey $pstrSvrPrivate;
+			$boards = Get-CWServiceBoard -Filter $filter -SizeLimit $sizeLimit -Server $pstrServer;
 			$boards | Measure-Object | Select -ExpandProperty Count | Should Be $sizeLimit;
 		}
 		
 		It 'gets boards and sorts board id by descending piping cmdlet through Sort-Object cmdlet' {
 			$boardIDs = $pstrBoardIDs;
-			$boards = Get-CWServiceBoard -BoardID $boardIDs -Domain $pstrSvrDomain -CompanyName $pstrSvrCompany -PublicKey $pstrSvrPublic -PrivateKey $pstrSvrPrivate | Sort -Descending id;
+			$boards = Get-CWServiceBoard -BoardID $boardIDs -Server $pstrServer | Sort -Descending id;
 			$maxBoardId = $boardIDs | Measure-Object -Maximum | Select -ExpandProperty Maximum
 			$boards[0].id | Should Be $maxBoardId;		
 		}
