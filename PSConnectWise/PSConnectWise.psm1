3b8f41ae763371afc88632b20b11998a71688db6
Brian <brian.k.ojeda@gmail.com>
2016-05-03 08:10:14 -0400

    removed redundent code

--- PSConnectWise/PSConnectWise.psm1
+++ PSConnectWise/PSConnectWise.psm1
@@ -1,6 +1,3 @@
-#dot-source import the classes
-. "$PSScriptRoot\Private\PSCWApiClasses.ps1"
-
 # Load and Export the Functions 
 #   Credit: https://github.com/RamblingCookieMonster/PSStackExchange
 
@@ -9,21 +6,14 @@
     $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
 #Dot source the files
-    Foreach($psfunction in @($Public + $Private))
+    Foreach($PSFunction in @($Public + $Private))
     {
         Try
         {
-            . $psfunction.fullname
+            . $PSFunction.FullName
         }
         Catch
         {
             Write-Error -Message "Failed to import function $($psfunction.fullname): $_"
         }
     }
-
-# Here I might...
-    # Read in or create an initial config file and variable
-    # Export Public functions ($Public.BaseName) for WIP modules
-    # Set variables visible to the module and its functions only
-
-Export-ModuleMember -Function $Public.Basename
